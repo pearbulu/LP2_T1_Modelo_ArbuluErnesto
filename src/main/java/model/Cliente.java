5f79df8668928ff35e6ca7bf778cde8cceccfd55
SUITE <pearbulu@cibertec.edu.pe>
2026-05-24 12:40:59 -0500

    Implementación de listado

---
+++ src/main/java/model/Cliente.java
@@ -0,0 +1,30 @@
+package model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tbl_cliente")
+@Getter
+@Setter
+public class Cliente {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_cliente")
+	private Integer idCliente;
+
+	@Column(name = "razon_social")
+	private String razonSocial;
+
+	@Column(name = "ruc")
+	private String ruc;
+
+}
---
+++ src/main/java/model/Tecnico.java
@@ -0,0 +1,45 @@
+package model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tbl_tecnico")
+@Getter
+@Setter
+public class Tecnico {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_tecnico")
+	private Integer idTecnico;
+
+	@Column(name = "nombre")
+	private String nombre;
+
+	@Column(name = "especialidad")
+	private String especialidad;
+
+	public String getEspecialidadDescripcion() {
+		switch (especialidad) {
+		case "S":
+			return "Software";
+		case "H":
+			return "Hardware";
+		case "R":
+			return "Redes";
+		case "B":
+			return "Base de Datos";
+		default:
+			return "Desconocido";
+		}
+	}
+
+}
